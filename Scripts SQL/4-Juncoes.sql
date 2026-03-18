7daa55aeeec8ea01ea7d4c8474c9743eb085b062
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2026-03-18 10:52:48 -0300

    Adicionei exemplos JOIN e agreguei consultas
    Scripts de atualização SQL/4-Juncoes.sql: adicione e esclareça exemplos de junção (RIGHT JOIN, FULL JOIN), uso do COALESCE e consultas agregadas. Inclui salário AVG por departamento com GROUP BY e uma variante filtrando contratações no dia = 10. Também pequena formatação/limpeza de comentários e linhas em branco adicionais para facilitar a leitura.

--- Scripts SQL/4-Juncoes.sql
+++ Scripts SQL/4-Juncoes.sql
@@ -26,6 +26,7 @@ select *
 from engineer03.projetos
 
 
+
 -- INNER JOIN - Nome e salário dos funcionários alocados em projetos.
 select
 	e.nome,
@@ -36,7 +37,8 @@ from engineer03.funcionarios e
 inner join engineer03.projetos p on e.id_funcionario = p.func_id;
 
 
--- Nome e salario de todos os funcionarios independentes de estarem alocados em projetos.
+
+-- LEFT JOIN - Nome e salario de todos os funcionarios independentes de estarem alocados em projetos.
 select
 	e.nome,
 	e.salario,
@@ -44,6 +46,7 @@ select
 	
 from engineer03.funcionarios e 
 left join engineer03.projetos p on e.id_funcionario = p.func_id;
+
 
 
 select
@@ -55,7 +58,8 @@ from engineer03.projetos p
 left join engineer03.funcionarios e on e.id_funcionario = p.func_id;
 
 
--- Alternativa mais elegante para o resultado anterior 
+
+-- COALESCE - Alternativa mais elegante para o resultado anterior 
 select
 	e.nome,
 	e.salario,
@@ -63,6 +67,64 @@ select
 
 from engineer03.funcionarios e
 left join engineer03.projetos p on e.id_funcionario = p.func_id;
+
+
+
+-- RIGHT JOIN - Nome de todos os funcionários alocados em projetos e os projetos sem funcionários alocados
+
+select
+	e.nome,
+	e.salario,
+	COALESCE(p.nome_projeto, 'Não Alocado em Projeto')
+
+from engineer03.funcionarios e
+right join engineer03.projetos p on e.id_funcionario = p.func_id;
+
+
+-- FULL JOIN - Nome de todos os funcionários alocados ou não em projetos e todos os projetos com ou sem funcionários alocados
+
+
+select
+	coalesce(e.nome, 'Sem Funcionário Alocado') as nome_funcionario,
+	coalesce(p.nome_projeto, 'Não Alocado em Projeto')
+
+from engineer03.funcionarios e
+full join engineer03.projetos p on e.id_funcionario = p.func_id;
+
+
+-- Média de salário dos departamentos com funcionários alocados em projetos
+
+select
+	f.departamento,
+	ROUND(AVG(f.salario), 2) as media_salarial_dpt
+	
+from engineer03.funcionarios f
+inner join engineer03.projetos p on f.id_funcionario = p.func_id
+group by f.departamento;
+
+
+
+/* Média de salário dos departamentos com funcionários alocados em projetos, cuja data de contratação do funcionário tenha sido 
+   no dia 10 de qualquer mês ou ano */ 
+
+select
+	f.departamento,
+	ROUND(AVG(f.salario), 2) as media_salarial_dpt
+
+from engineer03.funcionarios f
+inner join engineer03.projetos p on f.id_funcionario = p.func_id
+
+where extract(day from dt_contratacao) = 10
+group by f.departamento;
+
+
+
+
+
+
+
+
+
 
 
 
